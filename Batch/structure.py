d1a65c0d2b9e5335b1782f751815393f9220d403
HOME <kraynoff.i@yandex.ru>
2023-08-13 21:24:49 +0300

    start db

---
+++ Batch/structure.py
@@ -0,0 +1,48 @@
+import peewee as pe
+
+database_connection = pe.MySQLDatabase('comsol_solves',
+                                       user='root',
+                                       password='',
+                                       autoconnect=False)
+
+
+class BaseModel(pe.Model):
+
+    class Meta:
+        database = database_connection
+
+
+class Solves(BaseModel):
+    name = pe.CharField()
+    date = pe.DateTimeField(default=pe.datetime.datetime.now)
+    description = pe.CharField(100)
+
+
+class Coefs(BaseModel):
+    solve = pe.ForeignKeyField(Solves,
+                               related_name='fk_solves',
+                               on_delete='cascade',
+                               on_update='cascade')
+    type = pe.CharField(10),
+    value = pe.DecimalField()
+
+
+class Functions(BaseModel):
+
+    # class Meta:
+    #     primary_key = False
+
+    solve = pe.ForeignKeyField(Solves,
+                               related_name='fk_solves',
+                               on_delete='cascade',
+                               on_update='cascade')
+    name = pe.CharField(10),
+    time = pe.DecimalField(),
+    value = pe.DecimalField(),
+
+
+if __name__ == '__main__':
+    with database_connection:
+        classes = [Solves, Coefs, Functions]
+        database_connection.drop_tables(classes)
+        database_connection.create_tables(classes)
