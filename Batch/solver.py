360cec85fc77a3c9afc971e89f0b75a65e4284a5
HOME <kraynoff.i@yandex.ru>
2023-09-07 09:51:21 +0300

    get image from data

--- Batch/solver.py
+++ Batch/solver.py
@@ -1,6 +1,9 @@
 import os as _os
+import numpy as _np
 from tqdm import tqdm as _tqdm
 import pandas as _pd
+from scipy.interpolate import RBFInterpolator,griddata
+
 
 from structure import Const, Solve, db
 
@@ -100,26 +103,66 @@ def write(name, desc, df):
 
     return grouped_frames
 
+def get_solves(conditious):
+    string = 'select * from pivot \n where \n'
+    for key, diap in conditious.items():
+        string += f'{diap[0]} <= {key} and {key} <= {diap[1]} \n and \n'
+
+    querry = string[:-8]
+
+    with db:
+        columns = [i.name for i in db.get_columns('pivot')]
+        # columns.remove('data')
+        cursor = db.execute_sql(querry)
+        result = cursor.fetchall()
+    df = _pd.DataFrame(columns=columns, data=result)
+
+
+    datas = df['data']
+    data_df_list = []
+    for data in datas:
+        data_df_list.append( _pd.read_json(data[1:-1].replace('\\','')))
+
+    del df['data']
+    return df,data_df_list
+
+
+
+def flat2image(
+    x: _np.ndarray,
+    y: _np.ndarray,
+    z: _np.ndarray,
+    method='linear',
+    grid_points=11,
+    **kwargs,
+):
+    xi = _np.linspace(x.min(), x.max(), grid_points)
+    yi = _np.linspace(y.min(), y.max(), grid_points)
+    X, Y = _np.meshgrid(xi, yi)
+    if method in ['linear', 'cubic', 'nearest']:
+        Z = griddata(
+            points=(x, y),
+            values=z,
+            xi=(X, Y),
+            method=method,
+            **kwargs,
+        )
+
+    elif method == 'rbf':
+        XYi = _np.stack((X, Y))
+        XY_line = XYi.reshape(2, -1).T
+
+        interpol = RBFInterpolator(
+            _np.vstack((x, y)).T,
+            z,
+            **kwargs,
+        )
+        Z = interpol(XY_line).reshape(grid_points, grid_points)
+
+    return X, Y, Z
 
 def solve_and_write(name=None, desc=None):
     solve()
     df = df_from_comsol()
     df_list = write(name, desc, df)
     return df_list
-
-
-# df = df_from_comsol()
-# df.head()
-# a = write('test', 'test decs  ', df)
-# with database_connection:
-#     q = FuncData.select(FuncData).join(Solve).where(
-#         Solve.name == 'test_#11').get()
-
-# _pd.read_json(q.data)
-
-# class Solver:
-
-#     def __init__(self):
-#         pass
-
-#     def
