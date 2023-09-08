0ec59c47323729ce088614d45f6260b670265a2e
HOME <kraynoff.i@yandex.ru>
2023-09-08 14:16:46 +0300

    fixes, sweep done

--- Batch/solver.py
+++ Batch/solver.py
@@ -1,25 +1,119 @@
 import pandas as _pd
 import numpy as _np
+import re as _re
 from tqdm import tqdm as _tqdm
+import mph as _mph
 
-from scipy.interpolate import RBFInterpolator, griddata
-from structure import Const, Solve, db
+from itertools import product as _product
+from scipy.interpolate import RBFInterpolator as _RBFInterpolator
+from scipy.interpolate import griddata as _griddata
+from structure import Solve, db
 
-CONST_DEFAULT = {
-    'Ke': 1E+9,
-    'KH': 1E+9,
-    'Kr': 1E+9,
-    'Kdisp': 1E+9,
-    'KqH': 2000,
-    'Ks': 2,
-    'Kd': 0.05,
-    'Kc': 1,
-    'Kp': 0.001,
-    'KrD': 1E+9,
-    'Kph': 1E-5,
-}
+
+# Evaluating
+def model_parametrs(model: _mph.Model, changed_params: dict = {}):
+    for key, value in changed_params.items():
+        model.parameter(name=key, value=value)
+    return model.parameters()
+
+
+def evaluate_species(model: _mph.Model, outer_numbers, comsol_dataset='Data'):
+    reaction_node = model / 'physics' / 'Reaction Engineering'
+    _reaction_node_children = [i.name() for i in reaction_node.children()]
+
+    species = _re.findall(
+        string='\n'.join(_reaction_node_children),
+        pattern='Species: (.*)',
+    )
+    species_concentration_names = ['reaction.c_' + specie for specie in species]
+
+    if not isinstance(outer_numbers,list):outer_numbers  = [outer_numbers]
+
+    df = _pd.DataFrame(columns=['Time'] + species+['light'])
+    for outer_number in outer_numbers:
+        row_data = model.evaluate(
+            ['t'] + species_concentration_names,
+            dataset=comsol_dataset,
+            outer=outer_number,
+        )
+        temp_df = _pd.DataFrame(row_data, columns=['Time'] + species)
+        temp_df['light']= model.outer(comsol_dataset)[1][outer_number-1]
+
+        df = _pd.concat([df,temp_df])
+
+    return df
+
+def sweep(
+    model: _mph.Model,
+    variable_params,
+    name=None,
+    desc=None,
+):
+    name = check(name)
+    desc = check(desc)
+
+    params_list = _tqdm(iterable=variable_params)
+
+    i = 0
+    for changed_params in params_list:
+        model_parametrs(
+            model=model,
+            changed_params=changed_params,
+        )
+        model.clear()
+        params_list.set_description('{:10}'.format('Solving...'))
+        model.solve()
+        params_list.set_description('{:10}'.format('Saving...'))
+        solves_to_sql(
+            model=model,
+            name=name + f'#{i}',
+            desc=desc,
+        )
+        i += 1
+
 
 # SQL
+# TODO: logs
+def solves_to_sql(
+    model: _mph.Model,
+    name,
+    desc=None,
+):
+    _, light_sweep = model.outer('Data')
+    i = 1
+    params = {
+        key: float(value)
+        for key,
+        value in model.parameters().items()
+        if 'light' not in key
+    }
+    params['name'] = name
+    params['desc'] = desc
+
+    notes = []
+    for light_value in light_sweep:
+        note = {}
+        note.update(params)
+        note['light'] = light_value
+        df = evaluate_species(model, i)
+        note['data'] = df.to_json(index=True)
+        notes.append(note)
+
+        i += 1
+
+    with db:
+        Solve.insert_many(notes).execute()
+
+
+def solve_to_sql(df, params: dict, name, desc=None):
+    note = params.copy()
+    note['name'] = name
+    note['desc'] = desc
+    note['data'] = df.to_json(index=True)
+    with db:
+        Solve.insert(note).execute()
+
+
 def get_solves(conditious):
     string = 'select * from pivot \n where \n'
     for key, diap in conditious.items():
@@ -42,6 +136,7 @@ def get_solves(conditious):
     del df['data']
     return df, data_df_list
 
+
 # To plots
 def flat2image(
     x: _np.ndarray,
@@ -55,7 +150,7 @@ def flat2image(
     yi = _np.linspace(y.min(), y.max(), grid_points)
     X, Y = _np.meshgrid(xi, yi)
     if method in ['linear', 'cubic', 'nearest']:
-        Z = griddata(
+        Z = _griddata(
             points=(x, y),
             values=z,
             xi=(X, Y),
@@ -67,7 +162,7 @@ def flat2image(
         XYi = _np.stack((X, Y))
         XY_line = XYi.reshape(2, -1).T
 
-        interpol = RBFInterpolator(
+        interpol = _RBFInterpolator(
             _np.vstack((x, y)).T,
             z,
             **kwargs,
@@ -75,3 +170,19 @@ def flat2image(
         Z = interpol(XY_line).reshape(grid_points, grid_points)
 
     return X, Y, Z
+
+
+def check(string):
+    if string is None:
+        while string != 'q':
+            string = input(f'Set {string=}, to quit - q:')
+            string = string.strip()
+    return string
+
+
+def combinations_dict(diap: dict):
+    keys = list(diap.keys())
+    values = [diap[key] for key in keys]
+    combinations = list(_product(*values))
+    result = [dict(zip(keys, comb)) for comb in combinations]
+    return result
